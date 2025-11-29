cb703678eacb0ca3dabbfba13d96de3b9e29a9b7
circuit <oxcl@proton.me>
2025-11-29 19:32:55 +0330

    improve the flake logic to take the .ttf file as a flake input

--- default.nix
+++ default.nix
@@ -1,14 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }: with pkgs; stdenvNoCC.mkDerivation rec {
+{ pkgs ? import <nixpkgs> {}, apple_color_emoji_font }: with pkgs; stdenvNoCC.mkDerivation rec {
   pname = "apple-emoji-nix";
   version = "0.0.1";
-  src = fetchurl {
-    url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/v17.4/AppleColorEmoji.ttf";
-    hash = "sha256-SG3JQLybhY/fMX+XqmB/BKhQSBB0N1VRqa+H6laVUPE=";
-  };
   dontBuild = true;
   dontUnpack = true;
   installPhase = ''
-    install -Dm755 ${src} $out/share/fonts/truetype/AppleColorEmoji.ttf
+    install -Dm755 ${apple_color_emoji_font} $out/share/fonts/truetype/AppleColorEmoji.ttf
   '';
   meta = with lib; {
     description = "Apple Emoji font for linux";
--- flake.nix
+++ flake.nix
@@ -2,14 +2,18 @@
   description = "nix flake for apple-emoji-gtk";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    apple_color_emoji_font = {
+      url = "https://github.com/samuelngs/apple-emoji-linux/releases/latest/download/AppleColorEmoji.ttf";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, apple_color_emoji_font }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
-    packages.x86_64-linux.apple-emoji-nix = pkgs.callPackage ./default.nix { inherit pkgs; };
+    packages.x86_64-linux.apple-emoji-nix = pkgs.callPackage ./default.nix { inherit pkgs apple_color_emoji_font; };
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.apple-emoji-nix;
 
